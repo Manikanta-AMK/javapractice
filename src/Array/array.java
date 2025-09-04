157ca0827ebcd639dbd61654dacd48bb529e69bb
Manikanta-AMK <Manikantaarige319@gmail.com>
2025-09-04 09:41:33 +0530

    latest comment with new comment

--- src/Array/array.java
+++ src/Array/array.java
@@ -11,7 +11,7 @@ import java.util.Iterator;
 public class array {
 
 	public static void main(String[] args) {
-		
+		//new comment
 		// single dimentional array 
 		//type1 declareation
 		int a[] = new int[5];
